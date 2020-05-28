0d907626c71998c07b4a62794bcd0dc3fea8a668
Vladimir Lukyanov <vladimir@liikyanov.com>
2020-05-29 02:17:10 +0300

    v0.0.124

---
+++ js/js_0001.js
@@ -0,0 +1,39 @@
+let list = [10, 15, 3, 7], k = 17;
+
+//Solution #1
+function check_array(_array, sum) {
+
+    for (const element1 of _array) {
+        for (const element2 of _array) {
+            if (element1 + element2 === sum) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+//Solution #2
+function check_array2(_array, sum) {
+    let _diff_arr = [];
+    for (const element1 of _array) {
+        const difference = sum - element1;
+        if (_diff_arr.includes(difference)) {
+            return true;
+        }
+        _diff_arr.push(element1);
+    }
+    return false;
+}
+
+console.log(check_array(list, k));
+console.log(check_array([10, 20, 30, 40], 60));
+console.log(check_array([1, 5, 61, 10], 55));
+
+console.log(check_array2(list, k));
+console.log(check_array2([10, 20, 30, 40], 60));
+console.log(check_array2([1, 5, 61, 10], 55));
+
+
+console.log()
